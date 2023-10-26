4e36533e57a481b36ce01bd7b99eaaeb0bd532d0
jpscardoso97 <j040_k4rd050@hotmail.com>
2023-10-26 17:11:51 -0400

    Save progress

--- notes.md
+++ notes.md
@@ -6,3 +6,4 @@
 - Total number of subscriptions by account
 - Total number of individual tickets by account
 - Find correlation between categorical and numerical price.level
+- Multiple subscriptions only 73 yes against thousands of no (probably not worth using it)
--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -2,15 +2,41 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 def clean_data(subscriptions, accounts, concerts, planned_concerts, tickets, zipcodes):
+    label_encoder = LabelEncoder()
+
     print("Missing subscriptions data: ", subscriptions.isnull().sum(), "out of", len(subscriptions), "\n")
     print("Dropping rows with missing data...")
     print("Initial length of subscriptions: ", len(subscriptions))
     subscriptions = subscriptions.dropna()
     print("Length of subscriptions after cleaning: ", len(subscriptions))
 
+    print("Encoding subscriptions categorical columns...")
+    
+    sections_map = {
+    'Balcony Rear': 1, 
+    'Balcony': 2,
+    'Boxes House Right': 3, 
+    'Boxes House Left': 4, 
+    'Gallery': 5, 
+    'Floor': 6, 
+    'Balcony Front': 7, 
+    'Dress Circle': 8, 
+    'Orchestra Rear': 9, 
+    'Santa Rosa': 10, 
+    'Orchestra': 11, 
+    'Orchestra Front': 12, 
+    'Premium Orchestra': 13, 
+    'Box (Most Expensive)': 14}
+
+    subscriptions['section'] = subscriptions['section'].map(sections_map)
+
     print("Missing accounts data: ", accounts.isnull().sum(), "out of", len(accounts), "\n")
     print("Dropping rows with missing data...")
     print("Initial length of accounts: ", len(accounts))
+    
+    # Keep only amount.donated.2013,amount.donated.lifetime,no.donations.lifetime,first.donated columns on accounts
+    accounts = accounts[['account.id', 'amount.donated.2013', 'amount.donated.lifetime', 'no.donations.lifetime', 'first.donated', 'billing.zip.code']]
+    accounts['first.donated'] = pd.to_datetime(accounts['first.donated'])
     accounts = accounts.dropna()
     print("Length of accounts after cleaning: ", len(accounts))
 
@@ -36,5 +62,7 @@ def clean_data(subscriptions, accounts, concerts, planned_concerts, tickets, zip
     print("Length of tickets after cleaning: ", len(tickets))
 
     print("Missing zipcodes data: ", zipcodes.isnull().sum(), "out of", len(zipcodes), "\n")
+    zipcodes = zipcodes.dropna()
+    zipcodes['Zipcode'] = zipcodes['Zipcode'].astype(str)
 
     return subscriptions, accounts, concerts, planned_concerts, tickets, zipcodes
